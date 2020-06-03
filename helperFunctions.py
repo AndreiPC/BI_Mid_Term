6b6d0970e1b397ab6f6e30fa52a99c61614e5fbe
Pantea Andrei <andrei.pantea@contractors.roche.com>
2020-06-03 15:53:48 +0300

    included the actualy protein translation

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,33 +1,20 @@
 from proteinDictionary import Proteins
 
-#To find the ORF:
-    #Find start codon
-    #Translate to proteins until a stop codon is encountered
+def getProteinSequence(sequence):
+    startCDN = ['AUG']
+    stopCDN = ['UAA', 'UAG', 'UGA']
+    proteinList = []
 
-#Maybe keep in mind the index ? idk yet
-#Not sure if multiple start codons can be encountered.
+    startEncountered = False
+    stopTranscribing = False
 
+    for codon in sequence:
+        if codon in startCDN:
+            startEncountered = True
+        elif codon in stopCDN and startEncountered == True:
+            stopTranscribing = True
 
-startCDN = ['AUG']
-stopCDN = ['UAA', 'UAG', 'UGA']
-proteinList = []
-
-shortSequence = ['AUG','AAA', 'CUG', 'CUC', 'UUC', 'UUC', 'CUG', 'CUG','UGA','AUG']
-
-startEncountered = False
-stopTranscribing = False
-
-for codon in shortSequence:
-    if codon in startCDN:
-        startEncountered = True
-    elif codon in stopCDN and startEncountered == True:
-        stopTranscribing = True
-
-    if startEncountered == True and stopTranscribing != True:
-        proteinList.append(Proteins[codon])
-    
-
-print(proteinList)
-
-
-#BEFORE FULLY IMPLEMENTING THIS ! CHECK IF MULTIPLE START CODONS CAN EXIST IN A SEQUENCE.
+        if startEncountered == True and stopTranscribing != True:
+            proteinList.append(Proteins[codon])
+        
+    return proteinList
--- main.py
+++ main.py
@@ -9,12 +9,18 @@ def main(sourceSequence):
     dataObj.setRawData(sourceSequence)
     dataObj.transcriptToRNA()
     dataObj.setListOfCodons() 
+    print(helperFunctions.getProteinSequence(dataObj.Codons))
 
-    print(dataObj.Codons.count("AUG"))
-    print(dataObj.Codons)
 
 
 if __name__ == "__main__":
-    #file = input("Which file is the source ? ") 
-    #main(file + ".txt")
     main("source.txt")
+
+
+
+#To Do ?
+
+#Maybe ask the user to give the name of the file
+#Add extra steps to make sure the file is in the format I want it too.
+#Add checks in all methods to guard against bad input
+#Initialize the object directly with all the information (name, rawSequence, RNAsequence, ProteinSequence - maybe remove the codons since I only need them for 1 operation)
